940a51b0e987179c1a2868912b46b35f837da343
Tom Wu <twu@lcs.on.ca>
2023-11-01 11:57:30 -0400

    Initial Commit

---
+++ MolarMass/MolarMassApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolarMassApp.swift
+//  MolarMass
+//
+//  Created by Tom Wu on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct MolarMassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
